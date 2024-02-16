5fc0205d3aeff8613e5685aa369cea2fd9aaf9f1
serjsankov <serj.sankov@gmail.com>
2024-02-16 18:51:11 +0300

    ss

--- js/main.js
+++ js/main.js
@@ -2,87 +2,96 @@ window.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector(".header");
 
 
-  initClicks();
+    initClicks();
 
-  initMaskTel();
-  // initLoad();
-  // initScroolMagic();
-  initFancy();
-  initCustomSelect();
+    initMaskTel();
+    // initLoad();
+    // initScroolMagic();
+    initFancy();
+    initCustomSelect();
+    initSwiperMainBanner();
 
-  function initCustomSelect () {
-    $('select').select2({
-      width: '100%',
-  });
-  };
+    function initSwiperMainBanner() {
+        var swiperMainBanner = new Swiper(".swiper-main-banner", {
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        });
+    }
 
-
- function initFancy() {
-    Fancybox.bind("[data-fancybox]", {
-        // Your custom options
-      });
-  }
-
-
-  function initLoad() {
-      $(window).on("scroll", function () {
-          lazyLoad.img(200);
-          lazyLoad.bg(30);
-      });
-  }
-
-  function initScroolMagic() {
-      // init controller
-      const controller = new ScrollMagic.Controller();
-
-      // Создаём сцену 
-      new ScrollMagic.Scene({ triggerElement: "#list" })
-          .setClassToggle("#list", "active") // add class toggle
-          .addTo(controller);
-      new ScrollMagic.Scene({ triggerElement: "#equip" })
-          .setClassToggle("#equip", "active") // add class toggle
-          .addTo(controller);
-  }
-
-  function initMaskTel() {
-      // Маска для телефона
-      $.fn.setCursorPosition = function (pos) {
-          if ($(this).get(0).setSelectionRange) {
-              $(this).get(0).setSelectionRange(pos, pos);
-          } else if ($(this).get(0).createTextRange) {
-              var range = $(this).get(0).createTextRange();
-              range.collapse(true);
-              range.moveEnd('character', pos);
-              range.moveStart('character', pos);
-              range.select();
-          }
-      };
-      $.mask.definitions['~'] = '[49]';
-      $("input[type='tel']").click(function () {
-          $(this).setCursorPosition(2);
-      }).mask("+7 (~99) 999-99-99", {
-          placeholder: "_"
-      });
-  }
+    function initCustomSelect() {
+        $('select').select2({
+            width: '100%',
+        });
+    };
 
 
-  function initClicks() {
-      document.addEventListener("click", (event) => {
-          if (event.target.closest(".burger")) {
-              header.classList.toggle("active");
-              if (header.classList.contains("active")) { 
-                  document.body.style.overflow = "hidden";
-              } else {
+    function initFancy() {
+        Fancybox.bind("[data-fancybox]", {
+            // Your custom options
+        });
+    }
+
+
+    function initLoad() {
+        $(window).on("scroll", function () {
+            lazyLoad.img(200);
+            lazyLoad.bg(30);
+        });
+    }
+
+    function initScroolMagic() {
+        // init controller
+        const controller = new ScrollMagic.Controller();
+
+        // Создаём сцену 
+        new ScrollMagic.Scene({ triggerElement: "#list" })
+            .setClassToggle("#list", "active") // add class toggle
+            .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#equip" })
+            .setClassToggle("#equip", "active") // add class toggle
+            .addTo(controller);
+    }
+
+    function initMaskTel() {
+        // Маска для телефона
+        $.fn.setCursorPosition = function (pos) {
+            if ($(this).get(0).setSelectionRange) {
+                $(this).get(0).setSelectionRange(pos, pos);
+            } else if ($(this).get(0).createTextRange) {
+                var range = $(this).get(0).createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+        $.mask.definitions['~'] = '[49]';
+        $("input[type='tel']").click(function () {
+            $(this).setCursorPosition(2);
+        }).mask("+7 (~99) 999-99-99", {
+            placeholder: "_"
+        });
+    }
+
+
+    function initClicks() {
+        document.addEventListener("click", (event) => {
+            if (event.target.closest(".burger")) {
+                header.classList.toggle("active");
+                if (header.classList.contains("active")) {
+                    document.body.style.overflow = "hidden";
+                } else {
+                    document.body.style.overflow = "scroll";
+                }
+            }
+            if (event.target.closest(".header-menu__nav-link")) {
+                header.classList.remove("active");
                 document.body.style.overflow = "scroll";
-              }
-          }
-          if (event.target.closest(".header-menu__nav-link")) { 
-              header.classList.remove("active");
-              document.body.style.overflow = "scroll";
-          }
+            }
 
-      })
-  }
+        })
+    }
 })
 
 
